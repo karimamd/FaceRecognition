1f0b38cb90bea911632c33e4371f24f141cf2c73
ZyadShokry <zyadshokry.poe@gmail.com>
2018-03-03 17:21:01 +0200

    Trial for projecting training and test

--- faceRecognition.py
+++ faceRecognition.py
@@ -18,45 +18,44 @@ temp=np.arange(1,41,1)
 label_matrix=np.array([[temp[i]]*10 for i in range (temp.size)])
 label_matrix=label_matrix.flatten()
 
-def PCA_(data_matrix,alpha):
+def PCA_(data_matrix,alpha,char):
     isAlpha=False
     number=0
     data_matrix_centered= data_matrix - np.mean(data_matrix,axis=0)
     data_matrix_cov= np.cov(data_matrix_centered,rowvar=False)
     eigenValues, eigenVectors = LA.eigh(data_matrix_cov)
+    np.savez_compressed(char+"eigVal_0.8",eigenValues)
+    np.savez_compressed(char+"eigVec_0.8",eigenVectors)
     idx = eigenValues.argsort()[::-1]   
     eigenValues = eigenValues[idx]
     eigenVectors = eigenVectors[:,idx]
     total=np.sum(eigenValues)
-#    print(eigenValues, " eigVal\n",eigenVectors," eigVec\n")
     while(isAlpha==False):
         sum_eigVal=0.0
         for i in range (eigenValues.size):
             sum_eigVal=sum_eigVal+eigenValues[i]/total
             number+=1
-            if(sum_eigVal>=alpha):
+            if(math.isclose(sum_eigVal,alpha)|(sum_eigVal>alpha)):
                 isAlpha=True
                 break
             
     projection_matrix=np.matrix([eigenVectors[n] for n in range (number)]).T
-    print(projection_matrix,number)
     rd_data_matrix= np.matmul(data_matrix,projection_matrix)
     return rd_data_matrix
 
-#imgMat=np.concatenate((imgMat,imgMat2),axis=0)
-#print(imgMat)
-#
 for j in range(1,41):
     direction=folder+"s"+str(j)+"\\"
     for i in range (1,11):
         directory=direction+str(i)+ ".pgm"
-       # print(directory)
         image = (img.imread(directory)).T
         imageVect=np.asmatrix(image.flatten())
         imgMat=np.concatenate((imgMat,imageVect))
         
 training_data_matrix=imgMat[::2]
-test_data_matrxi=imgMat[::1]
+test_data_matrix=imgMat[::1]
+
+training_data_matrix_rd=PCA_(training_data_matrix,0.8,"training_data")
+test_data_matrix_rd=PCA_(test_data_matrix,0.8,"test_data")
         
 
                
