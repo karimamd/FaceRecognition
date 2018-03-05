380b9069d3deb05d9284aea650d3285de482d390
Kareem Ahmed Abdel-salam <karimamd95@yahoo.com>
2018-03-05 07:19:12 +0200

    checking KNN merge conflicts diffs

--- playground.py
+++ playground.py
@@ -9,7 +9,6 @@ Created on Wed Feb 28 22:11:12 2018
 import numpy as np
 import operator
 import faceRecognition
-import matplotlib.pyplot as plt
 
 count=1
 labelVector =[]
@@ -42,19 +41,7 @@ def Knn(trainingSet,label_vector,test,k):
             classes[response] = 1 #new classVote! add it to the dict
     sortedVotes = sorted(classes.items(), key=operator.itemgetter(1), reverse=True)#sort the class votes in decreasing order
     return sortedVotes[0][0] #return the most voted class number
-#list for getting the predictions for all test instances
-#for x in range(200):    #test all the test_data_matrix
-#    list_of_predictions.append(Knn(faceRecognition.training_data_matrix,labelVector,faceRecognition.test_data_matrix[x],3))
-#print(getAccuracy(list_of_predictions,labelVector),"%")
-k=[1,3,5,7]
-accuracy=[]
-for i in range(4):
-    list_of_predictions=[] 
-    for x in range(200):    #test all the test_data_matrix
-        list_of_predictions.append(Knn(faceRecognition.training_data_matrix,labelVector,faceRecognition.test_data_matrix[x],k[i]))
-    accuracy.append(getAccuracy(list_of_predictions,labelVector))
-    print(accuracy[i],"%")
-plt.plot(k,accuracy)
-plt.ylabel("accuracy in %")
-plt.xlabel("K")
-plt.show
+list_of_predictions=[] #list for getting the predictions for all test instances
+for x in range(200):    #test all the test_data_matrix
+    list_of_predictions.append(Knn(faceRecognition.training_data_matrix,labelVector,faceRecognition.test_data_matrix[x],3))
+print(getAccuracy(list_of_predictions,labelVector),"%")
